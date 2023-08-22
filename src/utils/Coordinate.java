c9a2b5c52884c19677a68a9d3013a3a5686f1f15
Saad Kalyati <saadk09@hotmail.com>
2023-08-22 17:27:19 -0400

    Modifiy 'substract' method

--- src/utils/Coordinate.java
+++ src/utils/Coordinate.java
@@ -1,6 +1,8 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Coordinate {
   private double[] c;
@@ -9,8 +11,8 @@ public class Coordinate {
     this.c = new double[]{x, y, z};
   }
 
-  public Vector subtract(Coordinate c) {
-    return new Vector(this.c[0] - c.c[0], this.c[1] - c.c[1], this.c[2] - c.c[2]);
+  public Vector3D subtract(Coordinate c) {
+    return new Vector3D(this.x() - c.x(), this.y() - c.y(), this.z() - c.z());
   }
 
   public double x() {
@@ -24,7 +26,6 @@ public class Coordinate {
   public double z() {
     return this.c[2];
   }
-
   public String toString() {
 
     return "( " + this.c[0] + ", " + this.c[1] + ", " + this.c[2] + " )";
